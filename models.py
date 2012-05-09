8c344c688a01425698d49168c49b46276de104b6
Boryslav Larin <brabadu@gmail.com>
2012-05-09 17:50:27 +0300

    Finished first version of Pravda grabber

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ Models for default project
 import datetime
 
 from mongoengine import (connect, Document, StringField, DateTimeField,
-    URLField, BooleanField)
+    URLField, BooleanField, IntField, ListField)
 
 connect('pravda')
 
@@ -14,11 +14,14 @@ class Post(Document):
     title = StringField(required=True)
     content = StringField(required=True)
     url = URLField()
-    published_date = DateTimeField()
+    tags = ListField()
+
+    published_date_str = StringField()
     grabbed_date = DateTimeField(default=datetime.datetime.now)
 
     title_red = BooleanField()
-    title_cappitalized = BooleanField()
     title_bold = BooleanField()
+    title_uppercased = BooleanField()
     has_photo = BooleanField()
     has_video = BooleanField()
+    comments_number = IntField()
--- spiders/pravda_archive.py
+++ spiders/pravda_archive.py
@@ -3,19 +3,83 @@ import datetime
 
 from spiders.base import BaseHubSpider
 from grab.spider import Task
+from grab.error import DataNotFound
+
+from .models import Post
 
 
 class PravdaArchiveSpider(BaseHubSpider):
     # initial_urls = ['http://www.pravda.com.ua/rus/archives']
-    base_url = 'http://www.pravda.com.ua/rus/archives'
+    base_url = 'http://www.pravda.com.ua/'
 
     def task_initial(self, grab, task):
         grab_date = datetime.date.today() - datetime.timedelta(1)
 
-        while self.items_total < 1000:
-            yield Task('archive_day', url='/date_{0}/'.format(grab_date.strftime('%d%m%Y')))
+        for i in xrange(30):
+            yield Task('archive_day', url='/rus/archives/date_{0}/'.format(grab_date.strftime('%d%m%Y')))
             grab_date -= datetime.timedelta(1)
-            self.items_total += 501
+            self.items_total += 1
+
+        print '=' * 20, 'Last day processed', grab_date + datetime.timedelta(1)
 
     def task_archive_day(self, grab, task):
-        print '\n'.join(el.get('href') for el in grab.xpath_list('//dd/a'))
+        dd = grab.xpath_list('//dl[@class="news4"]/dd')[0]
+        header_css_classes = dd.attrib.get('class', '').split(' ')
+        first_url = dd.find('a').get('href')
+        yield Task('news', url=first_url, header_css_classes=header_css_classes)
+
+    def task_news(self, grab, task):
+        title = grab.xpath_text('//h1[@class="title"]')
+        print title
+
+        content = grab.xpath_text('//div[@class="text"]')
+        print content
+
+        url = task.url
+        print url
+
+        tags = grab.xpath_list('//div[@class="text"]/p[@class="tags"]/a')
+        print ", ".join([t.text_content() for t in tags])
+
+        published_date = grab.xpath_text('//span[@class="dt2"]')
+        print published_date
+
+        header_css_classes = task.get('header_css_classes')
+        title_red = 'flash' in header_css_classes
+        print 'Red title:', title_red
+
+        title_bold = 'hl' in header_css_classes
+        print 'Bold title:', title_bold
+
+        title_uppercased = title.isupper()
+        print 'Upper title:', title_uppercased
+
+        try:
+            comments_number = int(grab.xpath_text('//div[@class="rpad"]/a[@class="but5"]'))
+        except (ValueError):
+            comments_number = None
+        print "Comments:", comments_number
+
+        has_photo = None
+        print 'Has photo:', has_photo
+
+        try:
+            has_video = bool(grab.xpath_text(grab.xpath_text('//div[@class="rpad"]/span[@class="stext"]/b')))
+        except (DataNotFound):
+            has_video = None
+        print 'Has video:', has_video
+
+        Post.objects.create(
+            title=title,
+            content=content,
+            url=url,
+            tags=tags,
+            published_date_str=published_date,
+            title_red=title_red,
+            title_bold=title_bold,
+            title_uppercased=title_uppercased,
+            has_video=has_video,
+            comments_number=comments_number,
+        )
+        print '=' * 20, 'Items total', self.items_total
+        grab.sleep(2, 10)
