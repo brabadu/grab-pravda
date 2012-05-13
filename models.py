925f602022065a4b1ce2dbe2d13815314eb660c4
Boryslav Larin <brabadu@gmail.com>
2012-05-13 16:48:11 +0300

    Extended Post models with couple of fields

--- models.py
+++ models.py
@@ -4,24 +4,33 @@ Models for default project
 """
 import datetime
 
-from mongoengine import (connect, Document, StringField, DateTimeField,
+from mongoengine import (connect, DynamicDocument, StringField, DateTimeField,
     URLField, BooleanField, IntField, ListField)
 
 connect('pravda')
 
 
-class Post(Document):
+class Post(DynamicDocument):
+    source = StringField(required=True)
+
     title = StringField(required=True)
     content = StringField(required=True)
     url = URLField()
     tags = ListField()
 
+    images = ListField()
+    links = ListField()
+
     published_date_str = StringField()
+    published_date = DateTimeField()
     grabbed_date = DateTimeField(default=datetime.datetime.now)
+
+    processed = ListField()
 
     title_red = BooleanField()
     title_bold = BooleanField()
     title_uppercased = BooleanField()
     has_photo = BooleanField()
     has_video = BooleanField()
+    updated = BooleanField()
     comments_number = IntField()
